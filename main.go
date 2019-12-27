ad2d761585668e61dd7577e2c37e7a77115c3f67
takumin <takumiiinn@gmail.com>
2019-12-27 19:50:25 +0900

    initial dhcp reply

--- main.go
+++ main.go
@@ -12,6 +12,26 @@ func handler(conn net.PacketConn, peer net.Addr, m *dhcpv4.DHCPv4) {
 	log.Println("conn", conn.LocalAddr())
 	log.Println("peer", peer)
 	log.Println("dhcpv4", m.Summary())
+
+	reply, err := dhcpv4.NewReplyFromRequest(m)
+	if err != nil {
+		log.Printf("NewReplyFromRequest failed: %v", err)
+		return
+	}
+
+	switch mt := m.MessageType(); mt {
+	case dhcpv4.MessageTypeDiscover:
+		reply.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeOffer))
+	case dhcpv4.MessageTypeRequest:
+		reply.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeAck))
+	default:
+		log.Printf("Unhandled message type: %v", mt)
+		return
+	}
+
+	if _, err := conn.WriteTo(reply.ToBytes(), peer); err != nil {
+		log.Printf("Cannot reply to client: %v", err)
+	}
 }
 
 func main() {
